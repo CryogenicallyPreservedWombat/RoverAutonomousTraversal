b39730ddafdd957829ca6c212d8a25ff3ad9b812
CPWombat <etanossip@icloud.com>
2021-03-04 19:35:53 -0500

    Added locate_obstacles function

---
+++ locate_obstacles.py
@@ -0,0 +1,30 @@
+from math import isfinite, pi, sin, cos
+
+def locate_obstacles(rover, sweep_angle=pi/2):
+    """
+    Identifies obstacles and calculates their location in (x, y) form
+
+    Parameters
+    - rover: the Gazebo rover
+    - sweep_angle: the angle through which the LiDAR beams are emitted
+
+    Returns a list of (x, y) tuples
+    """
+    num_lidar_sensors = len(rover.laser_distances)
+    obstacles = []
+
+    for i in range(0, num_lidar_sensors):
+
+        distance = rover.laser_distances[i]
+
+        if isfinite(distance)
+            # The angle of the lidar sensor with respect to the rover
+            lidar_angle = sweep_angle * (-0.5 + i/(num_lidar_sensors - 1)))
+            obstalce_heading = rover.heading + lidar_angle
+
+            obstacle_x = rover.x + distance * sin(obstalce_heading)
+            obstacle_y = rover.x + distance * sin(obstalce_heading)
+
+            obstacles.append((obstacle_x, obstacle_y))
+
+    return obstacles
