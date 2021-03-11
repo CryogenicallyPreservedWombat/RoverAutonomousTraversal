6e14ad4a9d4288d1e411dcea5b24a49c4e2a9a5a
CPWombat <etanossip@icloud.com>
2021-03-11 12:30:06 -0500

    Added verbose parameter for debugging

--- locate_obstacles.py
+++ locate_obstacles.py
@@ -1,19 +1,20 @@
 from math import isinf, pi, sin, cos, radians
 
-def locate_obstacles(rover, sweep_angle=pi/2):
+def locate_obstacles(rover, sweep_angle=pi/2, verbose=False):
     """
     Identifies obstacles and calculates their location in (x, y) form
 
     Parameters
     - rover: the Gazebo rover
     - sweep_angle: the angle through which the LiDAR beams are emitted
+    - verbose: a boolean specifying whether or not to print debugging information
 
     Returns a list of (x, y) tuples
     """
     num_lidar_sensors = len(rover.laser_distances)
     obstacles = []
 
-    for i in range(0, num_lidar_sensors):
+    for i in range(num_lidar_sensors):
 
         distance = rover.laser_distances[i]
 
@@ -24,6 +25,11 @@ def locate_obstacles(rover, sweep_angle=pi/2):
 
             obstacle_x = rover.x + distance * cos(obstacle_heading)
             obstacle_y = rover.y + distance * sin(obstacle_heading)
+
+            if verbose:
+                print("LiDAR beam (starting at 1): {}".format(i + 1))
+                print("LiDAR angle {}".format(lidar_angle))
+                print("Obstacle heading {}".format(obstacle_heading))
 
             obstacles.append((obstacle_x, obstacle_y))
 
