032ab28e7d495d90234f1e434beded7f74dd8d34
CPWombat <etanossip@icloud.com>
2021-03-14 23:28:17 -0400

    Changed default run to include verbose logs

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from run_course import run_course
 
 rover = Rover()
 # default value, client has not yet specified in what form the end coordinate will be provided
-run_course(rover, end_point=(10, 10))
+run_course(rover, end_point=(10, 10), verbose=True)
