867baf8ba0bd10b1048739cfa1ff290e10f90073
CPWombat <etanossip@icloud.com>
2021-04-03 12:54:31 -0400

    Removed main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from qset_lib import Rover
-from run_course import run_course
-
-rover = Rover()
-# default value, client has not yet specified in what form the end coordinate will be provided
-run_course(rover, end_point=(10, 10), verbose=True)
