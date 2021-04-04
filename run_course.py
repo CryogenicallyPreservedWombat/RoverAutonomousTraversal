d79169c0cb06d56bf283cc28a7cac286eb2da131
CPWombat <etanossip@icloud.com>
2021-04-04 18:45:05 -0400

    Changed default values and added buffer_distance parameter

--- run_course.py
+++ run_course.py
@@ -5,14 +5,14 @@ from grid_node import distance, neighbouring_nodes
 from grid import Grid
 from math import ceil
 
-def run_course(rover, end_point, node_spacing=1, include_diagonals=True, euclidean=True, verbose=False, sensors_to_ignore=[7], obstacle_padding=0.5):
+def run_course(rover, end_point, node_spacing=0.4, include_diagonals=True, euclidean=True, verbose=False, sensors_to_ignore=[7], obstacle_padding=0.4, buffer_distance=5.0):
     
     recalculate_route = False
     start_point = (rover.x, rover.y)
 
     # Might want to facilitate the initialization of a grid that fills
     # a larger area than is defined between the start and end points, just in case
-    grid = Grid.oversized_grid(start_point, end_point, node_spacing=node_spacing)
+    grid = Grid.oversized_grid(start_point, end_point, node_spacing=node_spacing, buffer_distance=buffer_distance)
 
     start_node = grid.nearest_node(start_point)
     end_node = grid.nearest_node(end_point)
