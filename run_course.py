5debacffd50af1b554b9bfa40ac9561c957ccf5f
CPWombat <etanossip@icloud.com>
2021-04-04 22:16:18 -0400

    Increased length of longest stitches; only prints waypoints on the stitched path

--- run_course.py
+++ run_course.py
@@ -11,6 +11,9 @@ def run_course(rover, end_point, node_spacing=0.4, include_diagonals=True, eucli
     recalculate_route = False
     start_point = (rover.x, rover.y)
 
+    # Ensures the stitched paths don't extend beyond LiDAR range (this is likely an underestimate--should be able to be as high as 15.0 / node_spacing)
+    max_stich_length = 10.0 / node_spacing
+
     # Might want to facilitate the initialization of a grid that fills
     # a larger area than is defined between the start and end points, just in case
     grid = Grid.oversized_grid(start_point, end_point, node_spacing=node_spacing, buffer_distance=buffer_distance)
@@ -19,7 +22,7 @@ def run_course(rover, end_point, node_spacing=0.4, include_diagonals=True, eucli
     end_node = grid.nearest_node(end_point)
 
     path = quickest_path(start_node, end_node, grid, include_diagonals=include_diagonals, euclidean=euclidean)
-    stitched_path = stitch_colinear_nodes(start_node, path)
+    stitched_path = stitch_colinear_nodes(start_node, path, maximum_stitch_size=max_stich_length)
 
     while len(stitched_path) != 0:
 
@@ -47,14 +50,14 @@ def run_course(rover, end_point, node_spacing=0.4, include_diagonals=True, eucli
             # might want to create an entirely new grid based on the current rover's position
             rover_node = grid.nearest_node((rover.x, rover.y))
             path = quickest_path(rover_node, end_node, grid, include_diagonals=include_diagonals, euclidean=euclidean, verbose=verbose)
-            stitched_path = stitch_colinear_nodes(rover_node, path)
+            stitched_path = stitch_colinear_nodes(rover_node, path, maximum_stitch_size=max_stich_length)
             recalculate_route = False
         
         if verbose:
             for row in grid._array:
                 for node in row:
                     node.is_rover = grid.nearest_node((rover.x, rover.y)) is node
-                    node.on_path = node in path
+                    node.on_path = node in stitched_path
             print(grid)
 
         next_node = stitched_path.pop(0)
