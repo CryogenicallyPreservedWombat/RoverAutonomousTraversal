91d8f8b2980902250604eb79374fe1d52e306bb6
CPWombat <etanossip@icloud.com>
2021-04-04 22:03:37 -0400

    Changed default values

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from run_course import run_course
 
 rover = Rover()
 end_point = (10, 10)
-node_spacing = 0.3
-obstacle_padding = 0.3
+node_spacing = 0.4
+obstacle_padding = 0.4
 
 run_course(rover, end_point, node_spacing=node_spacing, obstacle_padding=obstacle_padding, verbose=True)
